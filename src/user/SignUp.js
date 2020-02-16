173a2f0531fd6569980f2398e57d0b16f75aaeae
EunsooJung <hi.michael.jung@gmail.com>
2020-02-16 01:19:39 -0800

    Implemented user signup front-end and tested connect to server

--- src/user/SignUp.js
+++ src/user/SignUp.js
@@ -33,10 +33,44 @@ const SignUp = () => {
     success: false
   });
 
+  // To get the signup data, we can destructure states
+  const { name, email, password } = values;
+
   // To handle the states change status (higher order function), it returning another function)
   // blows, name is function, event is returning fuction
   const handleChange = name => event => {
     setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  // send json type data to backend using browser's fetch method by the default, also we can use axios instead of fetch method
+  const signup = user => {
+    // console.log(name, email, password);
+
+    // fetch method's first argument is url using 'API'
+    fetch(`${API}/signup`, {
+      // second arguement
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      // error handling
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  // click event handler
+  const signUpSubmit = event => {
+    // The browser doesn't reload when the button is clicked
+    event.preventDefault();
+    // call signup function to get the object data (name, email, password)
+    signup({ name, email, password });
   };
 
   const signUpForm = () => (
@@ -65,7 +99,7 @@ const SignUp = () => {
           className='form-control'
         />
       </div>
-      <Button type='primary' className='btn btn-primary'>
+      <Button onClick={signUpSubmit} type='primary' className='btn btn-primary'>
         Submit
       </Button>
     </Form>
@@ -79,7 +113,7 @@ const SignUp = () => {
     >
       {signUpForm()}
       {/* To check the states changing status, we can use json.stringity */}
-      {/* {JSON.stringify(values)} */}
+      {JSON.stringify(values)}
     </Layout>
   );
 };
