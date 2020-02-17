503f248c91c7222261b20ca7c8af0b7c5299e216
EunsooJung <hi.michael.jung@gmail.com>
2020-02-17 05:20:56 -0800

    Refactoring import statement

--- src/admin/addCategory.js
+++ src/admin/addCategory.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
